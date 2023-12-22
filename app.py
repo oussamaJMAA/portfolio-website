aeea14a2e5a50a53806dd57b6eb86621cdc6f788
oussama jmaa <67621280+oussamaJMAA@users.noreply.github.com>
2023-12-22 01:55:22 +0100

    fixing library issues

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from streamlit.components.v1 import html
-from st_on_hover_tabs import on_hover_tabs
 import requests
-from streamlit_lottie import st_lottie
 from PIL import Image
-import streamlit_analytics
 import base64
 from streamlit_extras.mention import mention
-from streamlit_extras.app_logo import add_logo
-import sqlite3
-from bs4 import BeautifulSoup
-from streamlit_extras.echo_expander import echo_expander
+
 
 #test
 
